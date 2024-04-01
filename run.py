243eeb3d7006dc4c6a3f5cac38495ff1c91753b1
Mike Lawlor <mikelawlorarb@outlook.com>
2024-04-01 13:37:00 +0000

    add login in function and logins txt file

--- run.py
+++ run.py
@@ -12,8 +12,42 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('witches_pantry')
 
-user_1 = SHEET.worksheet('user_1')
+def read_logins():
+    with open('logins.txt', 'r') as f:
+        contents = f.readlines()
+        
+        new_contents = []
+        
+        for line in contents:
+            fields = line.split(',')
+            fields[1] = fields[1].rstrip()
+            new_contents.append(fields)
+        
+        return(new_contents)
+logins = read_logins()       
+                                                                                                 
+def login():
+    ask_username = str(input('Username: '))
+    ask_password = str(input('Password: '))   
+    
+    logged_in = False
+    
+    for line in logins:
+        if line[0] == ask_username and logged_in == False:
+            if line[1] == ask_password:
+                logged_in = True
+                
+    if  logged_in == True:
+        print('Logged in successfully')
+        main()
+    else:
+        print('Username / Password is incorrect')
+        login()          
 
-data = user_1.get_all_values()
+def main():
+    print(' Welcome To Your Pantry')
+    
+login()
 
-print(data)
+                                                                                             
+                                                                                             
