b1f1f4b982b62cfe877896898641cb2b5c12aec4
Mike Lawlor <mikelawlorarb@outlook.com>
2024-04-02 15:52:14 +0000

    add item function

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import gspread
-from google.oauth2.service_account import Credentials
+from google.oauth2.service_account import Credentials 
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -12,7 +12,12 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('witches_pantry')
 
+"""
 def read_logins():
+    """
+
+    #funtion to read user name and password and split into two fields
+"""
     with open('logins.txt', 'r') as f:
         contents = f.readlines()
         
@@ -27,6 +32,11 @@ def read_logins():
 logins = read_logins()       
                                                                                                  
 def login():
+    """
+    
+    #funtion to enter user name and password to login
+    
+"""
     ask_username = str(input('Username: '))
     ask_password = str(input('Password: '))   
     
@@ -38,16 +48,38 @@ def login():
                 logged_in = True
                 
     if  logged_in == True:
-        print('Logged in successfully')
+        print('Logged in successfully, welcome to your pantry')
         main()
     else:
         print('Username / Password is incorrect')
-        login()          
+        login()  
+"""
+
+def add_item():
+
+    """
+    function to add item to list 
+    """
+    
+    print("Please enter item to pantry")
+    print("Enter Item name and date in non US format dd/mm/yyyy")
+    print("Example: Cheese,20/04/2023\n")
+
+    item_str = input("Enter:")
+    print(f"The item and date is {item_str}")
+
+
+
+"""
+function to see items 1 week, 2 weeks, ,3 weeks, 1 month 
+"""
 
 def main():
-    print(' Welcome To Your Pantry')
+    """
+    Run all main funtions
+    """ 
     
-login()
-
-                                                                                             
+    
+#login()
+add_item()                                                                                       
                                                                                              
